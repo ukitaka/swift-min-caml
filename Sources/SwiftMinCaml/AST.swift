97355d18a42e52d6a2b60c55f65fa7fd11f23589
ukitaka <yuki.takahashi.1126@gmail.com>
2018-06-11 21:23:42 +0900

    Add AST

---
+++ Sources/SwiftMinCaml/AST.swift
@@ -0,0 +1,17 @@
+//
+//  AST.swift
+//  SwiftMinCaml
+//
+//  Created by Yuki Takahashi on 2018/06/11.
+//
+
+import Foundation
+
+// Constants
+protocol Const {
+}
+
+/// Expressions
+protocol Expr {
+    
+}
