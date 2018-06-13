0ec3d452a62a07707325cd068ec5ff91acdeae8e
ukitaka <yuki.takahashi.1126@gmail.com>
2018-06-14 02:45:30 +0900

    Use libc

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 swift build &> /dev/null
 swift run > ./tmp/main.s
 nasm -f macho64 ./tmp/main.s
-ld -macosx_version_min 10.7.0 -o ./tmp/main ./tmp/main.o
+ld -macosx_version_min 10.7.0 -o ./tmp/main ./tmp/main.o -lc
 ./tmp/main
