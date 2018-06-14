d0190fcbbcacb44ccffb129502aca5a26e0ee0ac
ukitaka <yuki.takahashi.1126@gmail.com>
2018-06-14 12:32:18 +0900

    Use %d in printf

--- smoke/printf.s
+++ smoke/printf.s
@@ -1,11 +1,12 @@
 extern _printf
 global start
 section .data
-hello: db 'hello, world', 10
+hello: db 'hello, world %d', 10
 section .text
 start:
 sub rsp, 16
 mov rdi, hello
+mov rsi, 1234
 call _printf
 add rsp, 16
 mov rax, 1
