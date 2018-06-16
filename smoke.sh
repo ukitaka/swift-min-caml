58f67bbcf7b4882a8a45b6f0e53abe508e2d3a99
ukitaka <yuki.takahashi.1126@gmail.com>
2018-06-16 13:43:30 +0900

    Finally make it possible to use ./smoke.sh

--- smoke.sh
+++ smoke.sh
@@ -3,17 +3,12 @@ nasm -f macho64 ./lib/libmincaml.s
 function run_test() {
   nasm -f macho64 -o ./tmp/$1.o ./smoke/$1.s
   ld -macosx_version_min 10.7.0 -o ./tmp/$1 -lc ./lib/libmincaml.o ./tmp/$1.o
-  #filename=./tmp/$1.output
-  #./tmp/$1 > $filename
-  #while read line ; do
-  #  echo ${line}
-  #done < ${filename}
-
-   #if [ $OUTPUT = $2 ]; then
-   # echo "[PASS] $1"
-   #else
-   # echo "[FAIL] $1, expected: $2, got: $OUTPUT"
-   #fi
+  OUTPUT=$(./tmp/printf)
+  if [ $OUTPUT = $2 ]; then
+   echo "[PASS] smoke/$1"
+  else
+   echo "[FAIL] $1, expected: $2, got: $OUTPUT"
+  fi
 }
 
 run_test "printf" 2234
