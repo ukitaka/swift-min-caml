2d7f4f8ae079dcea0735cadb5e981d36bf4c1cb2
ukitaka <yuki.takahashi.1126@gmail.com>
2018-06-16 13:52:04 +0900

    Cleanup

--- run_compiler_tests.sh
+++ run_compiler_tests.sh
@@ -1,14 +1,14 @@
 nasm -f macho64 ./lib/libmincaml.s
 
-function run_test() {
-  nasm -f macho64 -o ./tmp/$1.o ./smoke/$1.s
+function run_asm_test() {
+  nasm -f macho64 -o ./tmp/$1.o ./CompilerTests/asm/$1.s
   ld -macosx_version_min 10.7.0 -o ./tmp/$1 -lc ./lib/libmincaml.o ./tmp/$1.o
-  OUTPUT=$(./tmp/printf)
+  OUTPUT=$(./tmp/$1)
   if [ $OUTPUT = $2 ]; then
-   echo "[PASS] smoke/$1"
+   echo "[PASS] CompilerTests/asm/$1"
   else
    echo "[FAIL] $1, expected: $2, got: $OUTPUT"
   fi
 }
 
-run_test "printf" 2234
+run_asm_test "call_print_int" 2234
