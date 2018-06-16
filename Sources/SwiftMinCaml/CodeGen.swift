fb782cdc43d3f1b2b11fd2e9b294c8c29dd42468
ukitaka <yuki.takahashi.1126@gmail.com>
2018-06-16 16:46:44 +0900

    Add `labelOf` function

--- Sources/SwiftMinCaml/CodeGen.swift
+++ Sources/SwiftMinCaml/CodeGen.swift
@@ -6,18 +6,35 @@
 //
 
 struct CodeGen {
+    private typealias Reg = NasmX64Builder.Reg
     private let b = NasmX64Builder()
     private let startLabel = "start"
     private let exitLabel  = "mincaml_exit"
+    
+    class Context { }
 
     func gen(expr: Expr) -> String {
         b.raw("global \(startLabel)")
         b.section(.text)
         b.globalLabel(startLabel)
         b.and(.rsp, -16) // 16byte alignment
+        genExpr(expr: expr, context: Context()) // body
         b.jmp(exitLabel)
         genExit()
         return b.code
+    }
+    
+    private func genExpr(expr: Expr, context: Context) {
+        switch expr {
+        case let .const(const: const):
+            return genConst(const: const, context: context)
+        default:
+            fatalError("Not implemented")
+        }
+    }
+    
+    private func genConst(const: Const, context: Context) {
+        
     }
     
     private func genCall(expr: Expr) {
@@ -39,15 +56,43 @@ struct CodeGen {
         b.xor(.rdi, .rdi)
         b.syscall()
     }
+    
+    private func labelOf(node: Expr) -> Int {
+        switch node {
+        case .var, .const:
+            return 1
+        case let .arithOps(ops: _, args: args):
+            let labels = args.map(labelOf)
+            let allTheSame = labels.allTheSame()
+            let max = labels.max() ?? 1
+            if allTheSame, max < Reg.regs.count {
+                return labels.first ?? 1
+            }
+            if labels.containsOnly(Reg.regs.count) {
+                return Reg.regs.count
+            }
+            fatalError("unreachable.")
+        default:
+            fatalError("Not implemented!")
+        }
+    }
 }
 
 class NasmX64Builder {
     enum Reg: String {
         case rsp
         case rbp
-        case rax
-        case rcx
-        case rdi
+        case rax // for return
+        case rdi // arg1
+        case rsi // arg2
+        case rdx // arg3
+        case rcx // arg4
+        case r8  // arg5
+        case r9  // arg6
+
+        static var regs: [Reg] {
+            return [.rdi, .rsi, .rdx, .rcx, .r8, .r9]
+        }
     }
     
     enum Section: String {
