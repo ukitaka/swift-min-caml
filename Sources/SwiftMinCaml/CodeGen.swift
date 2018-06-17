fb54798b5c0d772de0c2d2169355bd9b33826fd8
ukitaka <yuki.takahashi.1126@gmail.com>
2018-06-17 12:46:56 +0900

    Generate arith ops

--- Sources/SwiftMinCaml/CodeGen.swift
+++ Sources/SwiftMinCaml/CodeGen.swift
@@ -6,12 +6,28 @@
 //
 
 struct CodeGen {
-    private typealias Reg = NasmX64Builder.Reg
+    typealias Reg = NasmX64Builder.Reg
     private let b = NasmX64Builder()
     private let startLabel = "start"
     private let exitLabel  = "mincaml_exit"
     
-    class Context { }
+    class Context {
+        private var regInUse: Int = 0
+        func useReg() -> Reg {
+            defer {
+                regInUse = regInUse + 1
+            }
+            return Reg.regs[regInUse]
+        }
+        
+        func readReg(offset: Int) -> Reg {
+            return Reg.regs[regInUse - offset]
+        }
+        
+        func releaseReg() {
+            regInUse = regInUse - 1
+        }
+    }
 
     func gen(expr: Expr) -> String {
         b.raw("global \(startLabel)")
@@ -28,13 +44,46 @@ struct CodeGen {
         switch expr {
         case let .const(const: const):
             return genConst(const: const, context: context)
+        case let .arithOps(ops: ops, args: args):
+            return genArithOps(ops: ops, args: args, context: context)
         default:
             fatalError("Not implemented")
         }
     }
     
+    private func genArithOps(ops: ArithOps, args: [Expr], context: Context) {
+        //FIXME: Now support only binary operators.
+        let lhs = args.first!
+        let rhs = args.last!
+        if labelOf(node: lhs) > labelOf(node: rhs) {
+            genExpr(expr: lhs, context: context)
+            genExpr(expr: rhs, context: context)
+        } else {
+            genExpr(expr: rhs, context: context)
+            genExpr(expr: lhs, context: context)
+        }
+        switch ops {
+        case .add:
+            b.add(context.readReg(offset: 1), context.readReg(offset: 2))
+        case .sub:
+            b.sub(context.readReg(offset: 1), context.readReg(offset: 2))
+        case .mul:
+            b.mul(context.readReg(offset: 1), context.readReg(offset: 2))
+        case .div:
+            fatalError("Not implemented!")
+        }
+        context.releaseReg()
+    }
+    
     private func genConst(const: Const, context: Context) {
-        
+        switch const {
+        case let .integer(n):
+            b.mov(context.useReg(), n)
+        case .float(_):
+            fatalError("Not implemented")
+        case .bool(_):
+            fatalError("Not implemented")
+        }
     }
     
     private func genCall(expr: Expr) {
@@ -63,13 +112,16 @@ struct CodeGen {
             return 1
         case let .arithOps(ops: _, args: args):
             let labels = args.map(labelOf)
-            let allTheSame = labels.allTheSame()
             let max = labels.max() ?? 1
-            if allTheSame, max < Reg.regs.count {
-                return labels.first ?? 1
+            let count = labels.filter { $0 == max }.count
+            if count > 1, max < Reg.regs.count {
+                return max + 1
             }
-            if labels.containsOnly(Reg.regs.count) {
-                return Reg.regs.count
+            if count > 1, max == Reg.regs.count {
+                return max
+            }
+            if count == 1 {
+                return max
             }
             fatalError("unreachable.")
         default:
@@ -91,7 +143,7 @@ class NasmX64Builder {
         case r9  // arg6
 
         static var regs: [Reg] {
-            return [.rdi, .rsi, .rdx, .rcx, .r8, .r9]
+            return [.rax, .rdi, .rsi, .rdx, .rcx, .r8, .r9]
         }
     }
     
@@ -103,6 +155,8 @@ class NasmX64Builder {
     enum Inst: String {
         case mov
         case add
+        case sub
+        case mul
         case and
         case xor
         case jmp
@@ -149,6 +203,10 @@ class NasmX64Builder {
         self.inst(.mov, dst, src)
     }
     
+    func add(_ dst: Reg, _ src: Reg) {
+        self.inst(.add, dst, src)
+    }
+    
     func add(_ dst: Reg, _ src: Int) {
         self.inst(.add, dst, src)
     }
@@ -156,7 +214,15 @@ class NasmX64Builder {
     func add(_ dst: Reg, _ src: String) {
         self.inst(.add, dst, src)
     }
-    
+
+    func sub(_ dst: Reg, _ src: Reg) {
+        self.inst(.sub, dst, src)
+    }
+
+    func mul(_ dst: Reg, _ src: Reg) {
+        self.inst(.mul, dst, src)
+    }
+
     func and(_ dst: Reg, _ src: Int) {
         self.inst(.and, dst, src)
     }
