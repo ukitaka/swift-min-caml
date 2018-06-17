a4d2b5438ac5ca36d92fe1581681c4c5048130bb
ukitaka <yuki.takahashi.1126@gmail.com>
2018-06-17 13:20:19 +0900

    Clean up

--- Tests/SwiftMinCamlTests/CodeGenTests.swift
+++ Tests/SwiftMinCamlTests/CodeGenTests.swift
@@ -14,6 +14,29 @@ class CodeGenTest: XCTestCase {
         let input = "1+2*3"
         let exp = try! parser.run(sourceName: "test", input: input)
         let asm = CodeGen().gen(expr: exp)
-        print(asm)
+        let expected = """
+        extern print_int
+        global start
+        section .text
+        start:
+        and rsp, -16
+        mov rax, 3
+        mov rdi, 2
+        mul rdi
+        mov rdi, 1
+        add rax, rdi
+        mov rdi, rax
+        sub rsp, -16
+        call print_int
+        add rsp, 16
+        jmp mincaml_exit
+        mincaml_exit:
+        mov rax, 1
+        add rax, 0x2000000
+        xor rdi, rdi
+        syscall
+
+        """
+        XCTAssertEqual(asm, expected)
     }
 }
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 swift build &> /dev/null
 swift run > ./tmp/main.s
 nasm -f macho64 ./tmp/main.s
-ld -macosx_version_min 10.7.0 -o ./tmp/main ./tmp/main.o -lc
+nasm -f macho64 ./lib/libmincaml.s
+ld -macosx_version_min 10.7.0 -o ./tmp/main  ./lib/libmincaml.o ./tmp/main.o -lc
 ./tmp/main
