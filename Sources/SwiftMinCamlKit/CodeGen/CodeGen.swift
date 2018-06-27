481943397fc5b04da4859b6d1cf970ca6b675b22
ukitaka <yuki.takahashi.1126@gmail.com>
2018-06-27 22:21:06 +0900

    Cleanup CodeGen

--- Sources/SwiftMinCamlKit/CodeGen/CodeGen.swift
+++ Sources/SwiftMinCamlKit/CodeGen/CodeGen.swift
@@ -53,41 +53,36 @@ public struct CodeGen {
 
     private func genExpr(expr: Expr, context: Context) {
         switch expr {
-        case let .const(const: const):
-            return genConst(const: const, context: context)
-        case let .arithOps(ops: ops, args: args):
-            return genArithOps(ops: ops, args: args, context: context)
-        case let .let(varName: varName, bind: bind, body: body):
-            return genLet(varName: varName, bind: bind, body: body, context: context)
-        default:
-            fatalError("Not implemented")
-        }
-    }
-
-    private func genLet(varName: Var, bind: Expr, body: Expr, context: Context) {
-        //FIXME: implement.
-        genExpr(expr: bind, context: context)
-        genExpr(expr: body, context: context)
-
-    }
-
-    private func genArithOps(ops: ArithOps, args: [Expr], context: Context) {
-        //FIXME: Now support only binary operators.
-        let lhs = args.first!
-        let rhs = args.last!
-        if labelOf(node: lhs) > labelOf(node: rhs) {
-            genExpr(expr: lhs, context: context)
-            genExpr(expr: rhs, context: context)
-        } else {
-            genExpr(expr: rhs, context: context)
-            genExpr(expr: lhs, context: context)
-        }
-        switch ops {
-        case .add:
+        case let .int(int):
+            b.mov(context.useReg(), int)
+        case let .add(lhs: lhs, rhs: rhs):
+            if labelOf(node: lhs) > labelOf(node: rhs) {
+                genExpr(expr: lhs, context: context)
+                genExpr(expr: rhs, context: context)
+            } else {
+                genExpr(expr: rhs, context: context)
+                genExpr(expr: lhs, context: context)
+            }
             b.add(context.readReg(offset: 2), context.readReg(offset: 1))
-        case .sub:
+            context.releaseReg()
+        case let .sub(lhs: lhs, rhs: rhs):
+            if labelOf(node: lhs) > labelOf(node: rhs) {
+                genExpr(expr: lhs, context: context)
+                genExpr(expr: rhs, context: context)
+            } else {
+                genExpr(expr: rhs, context: context)
+                genExpr(expr: lhs, context: context)
+            }
             b.sub(context.readReg(offset: 2), context.readReg(offset: 1))
-        case .mul:
+            context.releaseReg()
+        case let .mul(lhs: lhs, rhs: rhs):
+            if labelOf(node: lhs) > labelOf(node: rhs) {
+                genExpr(expr: lhs, context: context)
+                genExpr(expr: rhs, context: context)
+            } else {
+                genExpr(expr: rhs, context: context)
+                genExpr(expr: lhs, context: context)
+            }
             if context.readReg(offset: 2) == .rax {
                 b.mul(context.readReg(offset: 1))
             } else {
@@ -97,33 +92,33 @@ public struct CodeGen {
                 b.mov(context.readReg(offset: 2), .rax)
                 b.pop(.rax)
             }
-        case .div:
-            fatalError("Not implemented!")
-        }
-        context.releaseReg()
-    }
-
-    private func genConst(const: Const, context: Context) {
-        switch const {
-        case let .integer(n):
-            b.mov(context.useReg(), n)
-        case .float:
-            fatalError("Not implemented")
-        case .bool:
+            context.releaseReg()
+        case let .let(name: name, bind: bind, body: body):
+            return genLet(name: name, bind: bind, body: body, context: context)
+        default:
             fatalError("Not implemented")
         }
     }
 
-    private func genCall(expr: Expr) {
-        guard let apply = expr.asApply else {
+    private func genLet(name: TypedVar, bind: Expr, body: Expr, context: Context) {
+        //FIXME: implement.
+        genExpr(expr: bind, context: context)
+        genExpr(expr: body, context: context)
+
+    }
+
+    private func genApp(expr: Expr) {
+        guard case let .app(function: function, args: args) = expr else {
             fatalError("`expr` must be Expr.apply but \(expr)")
         }
-        //FIXME: Support only Const.int arg now.
-        let num = apply.args.first!.asConst!.asInteger!
+        //FIXME: Support only .int arg now.
+        guard case let .some(.int(int)) = args.first else {
+            fatalError("Support only int arg for now.")
+        }
         //FIXME: spill out if needed
         //FIXME: support 2~ args
-        b.mov(.rdi, num)
-        b.call(apply.function.rawValue)
+        b.mov(.rdi, int)
+        b.call(function.rawValue)
     }
 
     private func genExit() {
@@ -136,152 +131,26 @@ public struct CodeGen {
 
     private func labelOf(node: Expr) -> Int {
         switch node {
-        case .var, .const:
+        case .var, .int, .bool, .float:
             return 1
-        case let .arithOps(ops: _, args: args):
-            let labels = args.map(labelOf)
-            let max = labels.max() ?? 1
-            let count = labels.filter { $0 == max }.count
-            if count > 1, max < Reg.regs.count {
-                return max + 1
+        case let .add(lhs: lhs, rhs: rhs),
+             let .sub(lhs: lhs, rhs: rhs),
+             let .mul(lhs: lhs, rhs: rhs),
+             let .div(lhs: lhs, rhs: rhs):
+            
+            let (left, right) = (labelOf(node: lhs), labelOf(node: rhs))
+            let maxLabel = max(left, right)
+            
+            if left == right, maxLabel < Reg.regs.count {
+                return maxLabel + 1
             }
-            if count > 1, max == Reg.regs.count {
-                return max
+            if left == right, maxLabel == Reg.regs.count {
+                return maxLabel
             }
-            if count == 1 {
-                return max
-            }
-            fatalError("unreachable.")
+            return maxLabel
         default:
             fatalError("Not implemented!")
         }
     }
 }
 
-class NasmX64Builder {
-    enum Reg: String {
-        case rsp
-        case rbp
-        case rax // for return
-        case rdi // arg1
-        case rsi // arg2
-        case rdx // arg3
-        case rcx // arg4
-        case r8  // arg5
-        case r9  // arg6
-
-        static var regs: [Reg] {
-            return [.rax, .rdi, .rsi, .rdx, .rcx, .r8, .r9]
-        }
-    }
-
-    enum Section: String {
-        case data
-        case text
-    }
-
-    enum Inst: String {
-        case mov
-        case add
-        case sub
-        case mul
-        case and
-        case xor
-        case jmp
-        case push
-        case pop
-        case call
-        case syscall
-    }
-
-    enum SystemCall: Int {
-        case exit  = 1
-        case write = 4
-    }
-
-    var code: String
-
-    init() {
-        self.code = ""
-    }
-
-    func raw(_ code: String) {
-        self.code += code + "\n"
-    }
-
-    func section(_ section: Section) {
-        self.raw("section .\(section)")
-    }
-
-    func inst(_ inst: Inst, _ dst: Any, _ src: Any) {
-        self.raw("\(inst.rawValue) \(dst), \(src)")
-    }
-
-    func globalLabel(_ label: String) {
-        self.raw("\(label):")
-    }
-
-    func mov(_ dst: Reg, _ src: Reg) {
-        self.inst(.mov, dst, src)
-    }
-
-    func mov(_ dst: Reg, _ src: SystemCall) {
-        self.inst(.mov, dst, src.rawValue)
-    }
-
-    func mov(_ dst: Reg, _ src: Int) {
-        self.inst(.mov, dst, src)
-    }
-
-    func add(_ dst: Reg, _ src: Reg) {
-        self.inst(.add, dst, src)
-    }
-
-    func add(_ dst: Reg, _ src: Int) {
-        self.inst(.add, dst, src)
-    }
-
-    func add(_ dst: Reg, _ src: String) {
-        self.inst(.add, dst, src)
-    }
-
-    func sub(_ dst: Reg, _ src: Reg) {
-        self.inst(.sub, dst, src)
-    }
-
-    func sub(_ dst: Reg, _ src: Int) {
-        self.inst(.sub, dst, src)
-    }
-
-    func mul(_ reg: Reg) {
-        self.raw("\(Inst.mul) \(reg)")
-    }
-
-    func and(_ dst: Reg, _ src: Int) {
-        self.inst(.and, dst, src)
-    }
-
-    func xor(_ dst: Reg, _ src: Reg) {
-        self.inst(.xor, dst, src)
-    }
-
-    func jmp(_ label: String) {
-        self.raw("\(Inst.jmp) \(label)")
-    }
-
-    func call(_ label: String) {
-        self.raw("\(Inst.call) \(label)")
-    }
-
-    func push(_ reg: Reg) {
-        self.raw("\(Inst.push) \(reg.rawValue)")
-    }
-
-    func pop(_ reg: Reg) {
-        self.raw("\(Inst.pop) \(reg.rawValue)")
-    }
-
-    func syscall() {
-        self.raw(Inst.syscall.rawValue)
-    }
-}
