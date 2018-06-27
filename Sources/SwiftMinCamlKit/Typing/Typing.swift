4a7b7279a3043705bcbe24e6352d0855f54e9401
ukitaka <yuki.takahashi.1126@gmail.com>
2018-06-27 14:17:37 +0900

    Impl unitifcation

--- Sources/SwiftMinCamlKit/Typing/Typing.swift
+++ Sources/SwiftMinCamlKit/Typing/Typing.swift
@@ -16,43 +16,56 @@ extension Type {
     }
 }
 
+typealias Context = [Var: Type]
+
 enum Typing {
     static func type(expr: Expr) -> TypedExpr {
         let constraintType = expr.constraintTyping()
-        return unify(typedExpr: constraintType)
+        var context: Context = [:] //TODO: add builtin functions
+        return unify(typedExpr: constraintType, context: &context)
     }
     
-    private static func unify(typedExpr: TypedExpr) -> TypedExpr {
+    private static func unify(typedExpr: TypedExpr, context: inout Context) -> TypedExpr {
+        precondition(typedExpr.type.isTypeVar)
         switch typedExpr {
-        case let .const(const, type):
-            precondition(type.isTypeVar)
+        case let .const(const, _):
             return .const(const: const, type: typed(const: const))
-        case let .arithOps(ops, args, type):
-            guard type.isTypeVar else { return typedExpr }
-            let lhs = unify(typedExpr: args.first!)
-            let rhs = unify(typedExpr: args.last!)
-            let concreteType = unify(lhs: lhs.type, rhs: rhs.type)
+        case let .arithOps(ops, args, _):
+            let lhs = unify(typedExpr: args.first!, context: &context)
+            let rhs = unify(typedExpr: args.last!, context: &context)
+            precondition(lhs.type == rhs.type)
             return .arithOps(ops: ops,
-                             args: [lhs.typed(type: concreteType), rhs.typed(type: concreteType)],
-                             type: concreteType)
-        case let .`if`(cond, ifTrue, ifFalse, type):
-            guard type.isTypeVar else { return typedExpr }
-            let c = unify(typedExpr: cond)
-            let t = unify(typedExpr: ifTrue)
-            let f = unify(typedExpr: ifFalse)
-            let concreteType = unify(lhs: t.type, rhs: f.type)
-            return .if(cond: c.typed(type: .bool),
-                       ifTrue: t.typed(type: concreteType),
-                       ifFalse: f.typed(type: concreteType),
-                       type: concreteType)
-        case let .`let`(varName, bind, body, type):
-            fatalError()
-        case let .`var`(variable, type):
-            fatalError()
-        case let .letRec(name, args, bind, body, type):
-            fatalError()
-        case let .apply(function, args, type):
-            fatalError()
+                             args: [lhs, rhs],
+                             type: lhs.type)
+        case let .`if`(cond, ifTrue, ifFalse, _):
+            let typedCond = unify(typedExpr: cond, context: &context)
+            let typedIfTrue = unify(typedExpr: ifTrue, context: &context)
+            let typedIfFalse = unify(typedExpr: ifFalse, context: &context)
+            precondition(typedCond.type == .bool)
+            precondition(typedIfTrue.type == typedIfFalse.type)
+            return .if(cond: typedCond,
+                       ifTrue: typedIfTrue,
+                       ifFalse: typedIfFalse,
+                       type: typedIfTrue.type)
+        case let .`let`(varName, bind, body, _):
+            let typedBind = unify(typedExpr: bind, context: &context)
+            context[varName] = typedBind.type
+            let typedBody = unify(typedExpr: body, context: &context)
+            return .let(varName: varName, bind: typedBind, body: typedBody, type: typedBody.type)
+        case let .`var`(variable, _):
+            guard let type = context[variable] else {
+                fatalError("Unknown variable: \(variable)")
+            }
+            return .var(variable: variable, type: type)
+        case .letRec: // let .letRec(name, args, bind, body, type):
+            fatalError("Not implemeneted yet")
+        case let .apply(function, args, _):
+            guard let functionType = context[function]?.asFunc else {
+                fatalError("Unknown function: \(function)")
+            }
+            let typedArgs = args.map { unify(typedExpr: $0, context: &context) }
+            precondition(typedArgs.map{ $0.type } == functionType.args)
+            return .apply(function: function, args: args, type: functionType.ret)
         case let .tuple(elements, type):
             fatalError()
         case let .readTuple(vars, bindings, body, type):
@@ -63,31 +76,6 @@ enum Typing {
             fatalError()
         case let .writeArray(array, index, value, type):
             fatalError()
-        }
-    }
-    
-    private static func unify(lhs: Type, rhs: Type) -> Type {
-        switch (lhs, rhs) {
-        case (.int, .int):
-            return .int
-        case (.int, .typeVar):
-            return .int
-        case (.typeVar, .int):
-            return .int
-        case (.float, .float):
-            return .float
-        case (.float, .typeVar):
-            return .float
-        case (.typeVar, .float):
-            return .float
-        case (.bool, .typeVar):
-            return .bool
-        case (.typeVar, .bool):
-            return .bool
-        case (.bool, .bool):
-            return .bool
-        default:
-            fatalError("cannot unify")
         }
     }
     
