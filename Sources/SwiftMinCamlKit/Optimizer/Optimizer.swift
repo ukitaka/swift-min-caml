d7745ffb421b7d38a9f424f5cbacfb7319f22a43
ukitaka <yuki.takahashi.1126@gmail.com>
2018-06-30 12:52:15 +0900

    Add kNormal

---
+++ Sources/SwiftMinCamlKit/Optimizer/Optimizer.swift
@@ -0,0 +1,8 @@
+//
+//  Optimizer.swift
+//  SwiftMinCamlKit
+//
+//  Created by Yuki Takahashi on 2018/06/30.
+//
+
+enum Optimizer { }
---
+++ Sources/SwiftMinCamlKit/Optimizer/kNormal.swift
@@ -0,0 +1,12 @@
+//
+//  kNormal.swift
+//  SwiftMinCaml
+//
+//  Created by Yuki Takahashi on 2018/06/30.
+//
+
+extension Optimizer {
+    func kNormal(expr: Expr) -> Expr {
+        return expr //TODO: impl
+    }
+}
